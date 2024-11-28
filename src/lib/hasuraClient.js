9e83561dbbe57b125519a5aa3f24136b4dbf1b1e
CHETcica <chetsadakorn2543@gmail.com>
2024-11-28 20:11:09 +0700

    fix env

--- src/lib/hasuraClient.js
+++ src/lib/hasuraClient.js
@@ -1,10 +1,10 @@
 import { GraphQLClient } from 'graphql-request';
 
-// const HASURA_ENDPOINT = process.env.HASURA_ENDPOINT;
-// const HASURA_ADMIN_SECRET = process.env.HASURA_ADMIN_SECRET;
+const HASURA_ENDPOINT = process.env.HASURA_ENDPOINT;
+const HASURA_ADMIN_SECRET = process.env.HASURA_ADMIN_SECRET;
 
-const HASURA_ENDPOINT = 'https://chet-todo.hasura.app/v1/graphql';
-const HASURA_ADMIN_SECRET = '1nXyy3HX9UBM4KKC1ixNOHA2Ih9IOV7DMo8W4q1QgSpGP5mhOdUwy137YuJ2Htej';
+// const HASURA_ENDPOINT = 'https://chet-todo.hasura.app/v1/graphql';
+// const HASURA_ADMIN_SECRET = '1nXyy3HX9UBM4KKC1ixNOHA2Ih9IOV7DMo8W4q1QgSpGP5mhOdUwy137YuJ2Htej';
 
 export const hasuraClient = new GraphQLClient(HASURA_ENDPOINT, {
   headers: {
